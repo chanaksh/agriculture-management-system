f766f7550878bbfed9605cb04bbbbf6edaba7e06
chanaksh <chanakshmetkar@gmail.com>
2024-01-22 03:38:22 +0530

    add all

--- javascript/landing.js
+++ javascript/landing.js
@@ -1,9 +1,10 @@
 let element = document.querySelector('#search-me');
 function display() {
     var element = document.querySelector('#dis');
+    let element1=document.querySelector('.accout-of-data');
     element.classList.add('opt-bar-display');
     element.classList.remove('opt-bar-not-display');
-
+    element1.classList.add('blur-me-black-shadow');
     var element = document.querySelector('#acc-data');
     element.classList.remove('opt-bar-display');
     element.classList.add('opt-bar-not-display');
@@ -11,15 +12,18 @@ function display() {
 
 function not_display() {
     var element = document.querySelector('#dis');
+    let element1=document.querySelector('.accout-of-data');
+    element1.classList.remove('blur-me-black-shadow');
     element.classList.remove('opt-bar-display');
     element.classList.add('opt-bar-not-display');
 }
 
 function account_display() {
     var element = document.querySelector('#acc-data');
+    let element1=document.querySelector('.accout-of-data');
     element.classList.add('opt-bar-display');
     element.classList.remove('opt-bar-not-display');
-
+    element1.classList.add('blur-me-black-shadow');
     var element = document.querySelector('#dis');
     element.classList.remove('opt-bar-display');
     element.classList.add('opt-bar-not-display');
@@ -27,8 +31,10 @@ function account_display() {
 
 function account_not_display() {
     var element = document.querySelector('#acc-data');
+    let element1=document.querySelector('.accout-of-data');
     element.classList.remove('opt-bar-display');
     element.classList.add('opt-bar-not-display');
+    element1.classList.remove('blur-me-black-shadow');
 }
 
 
